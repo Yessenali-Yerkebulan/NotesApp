580747b63eeef6533470575da047269f2fbba950
Yerkebulan <yerkebulanyessenali03@gmail.com>
2024-04-30 06:43:01 +0500

    event handling by creating a new note

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,24 @@
   import {ref}  from "vue"
 
   const showModal = ref(false)
-  const newNote = ref(" ")
+  const newNote = ref("")
+  const notes = ref([])
+
+  function getRandomColor() {
+    return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+  }
+
+  const addNote = () => {
+    notes.value.push({
+      id: Math.floor(Math.random() * 1000000),
+      text: newNote.value,
+      date: new Date(),
+      backgroundColor: getRandomColor()
+    })
+
+    showModal.value = false
+    newNote.value = ""
+  }
 
 </script>
 
@@ -11,7 +28,7 @@
     <div v-if="showModal" class="overlay">
       <div class="modal">
         <textarea v-model="newNote" name="note" id="note" cols="30" rows="10"></textarea>
-        <button>Add Note</button>
+        <button @click="addNote">Add Note</button>
         <button @click="showModal=false" class="close">Close</button>
       </div>
     </div>
